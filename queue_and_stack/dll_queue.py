491e68e61b293cb5f055c2b4a3ed4951f89caf69
kellswork <Bighead93+>
2019-10-15 15:52:49 +0100

    implement queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -2,19 +2,28 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
 # first in first out
 
 class Queue:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Why is our DLL a good choice to store our elements? because queue is easily implemented on a linked list
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # add the value to the tail of the DDL
+        self.storage.add_to_tail(value)
+        # increment the size
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # check if the size is greater than 0
+        if self.size > 0:
+            # decrement the size of the counter
+            self.size -= 1
+            # remove head of DLL and return it
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,6 +1,7 @@
-from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
 
 # last in first out, push and pop. a real stack only lets these two functions happen in them
 
@@ -8,14 +9,17 @@ sys.path.append('../doubly_linked_list')
 class Stack:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Why is our DLL a good choice to store our elements? because stack is easily implemented on a linked list
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # add the value to the head of the DDL
+        self.storage.add_to_head(value)
+        # increment the size
+        self.size += 1
 
     def pop(self):
-        pass
+
 
     def len(self):
-        pass
+        return self.size
